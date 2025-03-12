5b4425b9b952db0d31bf523bec5b9f028aae336a
JulianDesinger <riverasolartejulianandres@gmail.com>
2025-03-12 14:36:36 -0500

    arreglo footer y home

--- app-ventas/src/components/Footer.jsx
+++ app-ventas/src/components/Footer.jsx
@@ -1,6 +1,9 @@
 import facebookIcon from "../assets/facebook.png";
 import instagramIcon from "../assets/instagram.png";
 import emailIcon from "../assets/email.png";
+import whatsappIcon from "../assets/whatsapp.webp";
+
+
 
 const Footer = () => {
   return (
@@ -16,6 +19,14 @@ const Footer = () => {
         <a href="#" className="hover:opacity-75">
           <img src={emailIcon} alt="Contacto" className="w-6 h-6 inline" />
         </a>
+         {/* Botón flotante de WhatsApp */}
+      <a
+        href="https://wa.me/3234437801"
+        className="fixed bottom-4 right-8 rounded-full shadow-lg transition-transform duration-300 ease-out transform z-10 hover:scale-110 "
+      >
+        <img src={whatsappIcon} alt="WhatsApp" className="w-12 h-12" />
+      </a>
+        
       </div>
     </footer>
   );
